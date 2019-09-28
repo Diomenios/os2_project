42d75ec1d57d4946a88a9113f89182188ecc1c4d
EXSamus <55688566+EXSamus@users.noreply.github.com>
2019-09-28 15:41:43 +0200

    PRNG + Tour
    Creation d'un PRNG: fini
    Generation d'un tour sur 3 secteur: fini

--- main.c
+++ main.c
@@ -29,28 +29,40 @@ int status;
 
 };
 
- int randomF();
 
 int numeroVoiture[20] = {7, 99, 5, 16, 8, 20, 4, 55, 10, 26, 44, 77, 11, 18, 23, 33, 3, 27, 63, 88};
 
 int main()
 {
     printf("Hello world!\n");
-    randomF();
-
+    tour();
     return 0;
 }
 
-int randomF(){
-    int i = 0;
-    for(int j = 0; j<10;j++){
 
-        i = rand()%5;
-        printf("%d \n",i);
+// generer un PRNG entre Min et Max
+int my_rand(int min, int max){
+  int c = rand()%(max-min+1)+min;//creation du nombre aleatoire
+  return c;
+}
+//temps d'un secteur
+int secteur(){
+    int temp = 0;
+    temp = my_rand(50, 600);//Generation du nombre aleatoire
+    printf("tour en %ds\n",temp);
+    return temp;
+}
+int tour(){
 
+    srand(time(NULL));
+    int i;
+    int total=0;
+    for(i=0; i<3;i++){
+        total +=secteur();
     }
-
-
+    int min = total%3600/60;
+    int sec = total%60;
+    printf("temps total est de %dmin %ds\n",min, sec);
 
 
 }

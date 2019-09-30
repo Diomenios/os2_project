2b8f1f4f26af4b132c0c23d497577fdacb6544f0
diomenios <loui.arys@gmail.com>
2019-09-30 19:38:05 +0200

    ajout d'un programme voiture_test.c + modification de main.c pour ajouter des execvp ()

--- main.c
+++ main.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "secteurs.h"//utilisation du header secteurs.h
-#include "voiture.h"//utilisation du header voiture.h
-#include "circuit.h" //utilisation du header circuit.h
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
-#include <windows.h>
+//#include <windows.h>
 #include <sys/types.h>
 #define true 1  //permettre l'utilisation du terme "true"
 #define false 0 //permettre l'utilisation du terme "false"
 
 
+void forkBomb(int decrem, pid_t cpid[]);
 
+/*  argv[0] doit être le nombre de voitures lancées sur le circuit
+*
+*/
+int max;
 
+int main(int argc, char *argv[]){
 
-int main()
-{
-    srand(time(NULL));//initialisation PRNG
-    printf("Bienvenue sur le projet C du groupe TEL (Tom-Emile-Louis)!\n");
-    tour();//appel de la m�thode tour
-    return 0;
+  max = 5;
+  //(int)*argv[0]
+  pid_t cpid[5];
+
+  forkBomb(5, cpid);
+
+  sleep(2);
+
+  exit(EXIT_SUCCESS);
 }
 
+void forkBomb(int decrem, pid_t cpid[]) {
+  if (decrem == 0) {
+    return ;
+  }
+   cpid[max-decrem] = fork();
+
+   if(cpid[max-decrem] == -1){
+     perror("fork");
+     exit(EXIT_FAILURE);
+   }
+
+   //child process
+   if (cpid[max-decrem] == 0) {
+     printf("vous êtes dans la voiture numéro : %d \n", max-decrem);
+     char *voiture[]= {"./voiture_test", NULL};
+     execvp(voiture[0], voiture); // execute the command pass in argument
+   }
+   //parent process
+   else{
+     forkBomb(decrem-1, cpid);
+   }
+
+}
---
+++ voiture_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+//#include <windows.h>
+
+int main(int argc, char* argv[]){
+  printf("%s\n", "je suis un bon fils");
+}

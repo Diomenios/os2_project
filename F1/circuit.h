e2fdaeb74e25119cbf4312a0f4334b9c236f8a03
EXSamus <55688566+EXSamus@users.noreply.github.com>
2019-10-07 19:24:32 +0200

    finalisation de la course et des qualif
    Course et qualif/essai terminé, ajout d'un petit affichage stylisé

--- F1/circuit.h
+++ F1/circuit.h
@@ -20,74 +20,82 @@ int tour(){
     int s = 0;//temps pour un secteur
     int i = 1;
     while (i<=3 && crash != true){
-
-        s = secteur(90,250);
+        s = secteur(100,250);
         Sleep(s*10);// endormir le processus pendant s*10 milliseconde
         if(s == 0){//test si il y a un crash
             crash = true;
-            printf("crash sur le secteur %d\n",i);
+            //printf("crash sur le secteur %d\n",i);
             return 0;
         }
         if((i%2)==0){//si il passe dans le secteur 2
            // k.tempSecteur2=s;
-           printf("temps du secteur %d: %d seconde\n",i, s);
+           printf(" %d       ||", s);
            //recuperer le temps ici
         }
         else if((i%3)==0){//si il passe dans le secteur 3
            // k.tempSecteur3=s;
            if(stand()){
                 total += 15;
-                printf("passage au Stand: +20 seconde\n");
+                //printf("passage au Stand: +20 seconde\n");
+                Sleep(200);// endormir le processus pendant s*10 milliseconde
            }
-           printf("temps du secteur %d: %d seconde\n",i, s);
+           printf(" %d       \n", s);
            //recuperer le temps ici
         }
         else{//si il passe dans le secteur 1
            // k.tempSecteur1=s;
-           printf("temps du secteur %d: %d seconde\n",i, s);
+           printf(" %d       ||", s);
            //recuperer le temps ici
         }
 
 
         total += s;//ajout au temps total de la voiture dans le circuit
+
         i++;
         //recuperer le temps total ici
-
     }
+
     return total;
 }
-void essaiLibre(int chrono){
+void essaiLibreQuali(int chrono){
     int temps1 = 0;
-    int temps2 = -1;
+    int temps2 = 0;
     int j = 1;
     do{
+        if(j>=10){
+            printf(" %d   ||", j);
+        }
+        else{
+            printf(" %d    ||", j);
+        }
+        if(temps2>=1000){
+            printf(" %d        ||",temps2);
+        }
+        else if(temps2>=100){
+            printf(" %d         ||",temps2);
+        }
+        else{
+            printf(" %d           ||",temps2);
+        }
+
         temps1 = tour();
         temps2 += temps1;
+
         if(temps1==0){
             printf("retour au stand: crash\n");
         }
-        else{
-            printf("temps du tour %d: %d seconde\n",j, temps2);
+        else if(temps2>=chrono){
+            printf(" %d   ||",temps2);
+            j++;
+        }
+        else {
+
+            affichage(j);
             j++;
         }
     }while(temps2<chrono && temps1 !=0);
 }
-int qualification(int chrono){
-    int temps1 = 0;
-    int temps2 = -1;
-    int j = 1;
-    do{
-        temps1 = tour();
-        temps2 += temps1;
-        if(temps1==0){
-            printf("retour au stand: crash\n");
-        }
-        else{
-            printf("temps du tour %d: %d seconde\n",j, temps2);
-            j++;
-        }
-    }while(temps2<chrono && temps1 !=0);
-}
+
 int Course(int tours){
     int temps1 = 0;
     int temps2 = -1;
@@ -103,5 +111,6 @@ int Course(int tours){
             printf("temps du tour %d: %d seconde\n",j, temps2);
             j++;
         }
-    }while(i<tours && temps1 !=0);
+    }while(i<=tours && temps1 !=0);
 }
+
--- F1/main.c
+++ F1/main.c
@@ -3,11 +3,13 @@
 #include "secteurs.h"//utilisation du header secteurs.h
 #include "voiture.h"//utilisation du header voiture.h
 #include "circuit.h" //utilisation du header circuit.h
+#include "affichage.h" //utilisation du header affichage.h
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
 #include <windows.h>
 #include <sys/types.h>
+
 #define true 1  //permettre l'utilisation du terme "true"
 #define false 0 //permettre l'utilisation du terme "false"
 
@@ -19,7 +21,9 @@ int main()
 {
     srand(time(NULL));//initialisation PRNG
     printf("Bienvenue sur le projet C du groupe TEL (Tom-Emile-Louis)!\n");
-    essaiLibre(5400);//appel de la méthode tour
+    printf(" Tour || Temps Total || Secteur 1 || Secteur 2 || Secteur 3\n");
+    essaiLibreQuali(5400);//appel de la méthode tour
+
     return 0;
 }
 

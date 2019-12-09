8c35759c9993ff1ccf9cd17a8c3fb5e024fbb74e
EXSamus <55688566+EXSamus@users.noreply.github.com>
2019-12-09 10:36:55 +0100

    helper malloc

--- helper.c
+++ helper.c
@@ -3,15 +3,19 @@
 #endif // HELPER_C_INCLUDED
 
 char* convertion(int temps){
+	
   int hours,minutes,seconds;
   int secondeRestante;
+  char* phrase = (char*) malloc(sizeof(char)* 40);
   int secondsInHour = 3600;
   int secondsInMinute = 60;
+  
   hours = (temps/secondsInHour); //Nombre d'heure
   secondeRestante = temps - (hours * secondsInHour); //Temps restant sans les heurs
   minutes = secondeRestante/secondsInMinute; //Nombre minutes
   secondeRestante = secondeRestante - (minutes*secondsInMinute); //Temps restant sans les minutes
   seconds = secondeRestante; //Nombre de secondes
-  printf("%d hour, %d minutes and %d seconds",hours,minutes,minutes);
-  //Malloc ??
+  
+  sprintf(phrase,"%d hour, %d minutes and %d seconds",hours,minutes,secondes); 
+  return phrase;
 }
--- helper.h
+++ helper.h
@@ -1,7 +1,10 @@
 #ifndef HELPER_H_INCLUDED
 #define HELPER_H_INCLUDED
 
-
-
-#endif // HELPER_H_INCLUDED
+/**
+*@param temps en seconde
+*@return un string qui est equivalent au temps en parametre (seconde) en heure/minute/seconde
+*/
 char* convertion(int temps);
+#endif // HELPER_H_INCLUDED
+
